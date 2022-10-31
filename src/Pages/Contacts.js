2ca05fcf922be67675f59149935b04744776521c
middleware <malifarge.thomas9@gmail.com>
2022-10-31 10:52:46 +0100

    delete useless info

--- src/Pages/Contacts.js
+++ src/Pages/Contacts.js
@@ -26,11 +26,6 @@ const Contacts = () =>{
             <H2>Contacts</H2>
             <address className="flex clmn g-50">
                 <div className="flex jcsb clmntel second">
-        
-                    <div className="flex center g-20">
-                        <img src={home} alt="home"/>
-                        <a target="_blank" rel="noreferrer" href="https://www.google.com/maps/place/95+Rue+Jean+Jaur%C3%A8s,+77270+Villeparisis/@48.9386518,2.6030413,17z/data=!3m1!4b1!4m5!3m4!1s0x47e6172f2a3b30ef:0x207d96bc136875d8!8m2!3d48.9386483!4d2.60523"> 95 rue jean jaurès , 77270 Villeparisis.Chez Mr.Baron</a>
-                    </div>
 
                     <div className="flex center g-20">
                         <img src={phone} alt="phone"/>
